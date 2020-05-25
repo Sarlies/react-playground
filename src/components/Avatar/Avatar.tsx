66b440987a23b60d3edab5ca82519e982b322e32
Sharlyne <digiderma@gmail.com>
2020-05-25 21:45:16 +0100

    Use typescript for props

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-import PropTypes from "prop-types";
-
 import "./styles.css";
 
-export default function Avatar({ image, name, size, type, loading }) {
+interface Props {
+    name: string,
+    image?: string,
+    loading?: boolean,
+    size?: "small" | "medium" | "large",
+    type?: "circle" | "rounded",
+}
+
+export default function Avatar({ image, name, size = "medium", type = "circle", loading }: Props) {
   return (
-    <div title={name} className={`avatar avatar-size--${size} avatar-type--${type}`}>
+    <div title={name} className={`avatar-size--${size} avatar-type--${type}`}>
         { loading ? 
             (<div className="avatar--loading"></div>) : 
             (image ? <img alt={name} src={image} /> : <div className="avatar-initials">{name.charAt(0)}</div>)
@@ -14,16 +20,3 @@ export default function Avatar({ image, name, size, type, loading }) {
     </div>
   );
 }
-
-Avatar.propTypes = {
-  loading: PropTypes.bool,
-  name: PropTypes.string.isRequired,
-  size: PropTypes.oneOf(["small", "medium", "large"]),
-  image: PropTypes.string,
-  type: PropTypes.oneOf(["circle", "rounded"]),
-};
-
-Avatar.defaultProps = {
-  size: "medium",
-  type: "circle",
-};
